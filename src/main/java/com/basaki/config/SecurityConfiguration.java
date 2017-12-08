d7bdb98569677ae10d8c3d86441dbf10b4b273c7
Indra Basak <indra.basak1@gmail.com>
2017-12-08 12:28:08 -0800

    Formatting login page layout

--- src/main/java/com/basaki/config/SecurityConfiguration.java
+++ src/main/java/com/basaki/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -32,7 +33,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll()
+        http.authorizeRequests().antMatchers("/hawtio", "/swagger-ui.html").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login")
                 .failureUrl("/login?error")
@@ -42,5 +43,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         "/hawtio/auth/logout/*"))
                 .logoutSuccessUrl("/login?logout")
                 .and().csrf().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+        web.ignoring().antMatchers("/resources/static/**");
+        web.ignoring().antMatchers("/webjars/**");
+        web.ignoring().antMatchers("/css/**");
     }
 }
--- src/main/java/com/basaki/config/SpringMvcConfiguration.java
+++ src/main/java/com/basaki/config/SpringMvcConfiguration.java
@@ -1,6 +1,7 @@
 package com.basaki.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -17,5 +18,14 @@ public class SpringMvcConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
     }
 }
